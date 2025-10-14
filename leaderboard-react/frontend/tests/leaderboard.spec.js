d3b7d9c9dbf8f0834c8d073aa9d2526bb587ebbc
Joshua Helm <joshuahelm088@gmail.com>
2025-10-13 19:12:52 -0700

    Make tests actually follow Page Object Model

--- leaderboard-react/frontend/tests/leaderboard.spec.js
+++ leaderboard-react/frontend/tests/leaderboard.spec.js
@@ -5,27 +5,42 @@ test('loads and displays entries', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open();
 
-    await leaderboard.hasVisibleScores();
+    const title = await leaderboard.getTitleText();
+    expect(title).toMatch(/^\d+ - .*/);
+
+    const scores = await leaderboard.getScoresText();
+    expect(scores[0]).toContain(':');
 });
 
 test('entries display proper time format', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open();
 
-    await leaderboard.scoresDisplayProperTimeFormat();
+    const scores = await leaderboard.getScoresText();
+    const count = await scores.length;
+    for (let i = 0; i < count; i++) {
+        const text = await scores[i];
+        expect(text.match(/:^([0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/));
+    }
 });
 
 test('scores are sorted by time ascending', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open();
 
-    await leaderboard.isSortedAscending();
+    const scores = await leaderboard.getScoresText();
+    const times = scores.map(s => parseInt(s.split(':')[0], 10));
+    for (let i=0; i<times.length - 1; i++) {
+        expect(times[i]).toBeLessThanOrEqual(times[i + 1]);
+    }
 });
 
 test('can load specified mission id', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open(1);
-    leaderboard.titleHasMissionId(1);
+
+    const firstTitle = await leaderboard.getTitleText();
+    expect(firstTitle).toMatch(/^1 - .*/);
 
     let min = 2;
     let max = await leaderboard.getLastMissionId();
@@ -33,25 +48,28 @@ test('can load specified mission id', async ({ page }) => {
     console.log(`random mission: ${randMission}`);
 
     await leaderboard.open(randMission);
-    await leaderboard.titleHasMissionId(randMission);
-
-    await page.pause();
+    const secondTitle = await leaderboard.getTitleText();
+    expect(secondTitle).toMatch(new RegExp(`^${randMission} - .*`));
 });
 
 test('previous button does not decrement when on first mission', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open();
 
-    await leaderboard.onFirstMission();
-    await leaderboard.prevBtnDoesNotDecrement();
+    const firstTitle = await leaderboard.getTitleText();
+    await leaderboard.clickPrevBtn();
+    const afterTitle = await leaderboard.getTitleText();
+    expect(afterTitle).toBe(firstTitle);
 });
 
 test('next button does not increase when on last mission', async ({ page }) => {
     const leaderboard = new LeaderboardPage(page);
     await leaderboard.open(await leaderboard.getLastMissionId());
 
-    await leaderboard.onLastMission();
-    await leaderboard.nextBtnDoesNotIncrement();
+    const firstTitle = await leaderboard.getTitleText();
+    await leaderboard.clickNextBtn();
+    const afterTitle = await leaderboard.getTitleText();
+    expect(afterTitle).toBe(firstTitle);
 });
 
 // test('can cycle through all missions', async ({ page }) => {
--- leaderboard-react/frontend/tests/pages/leaderboard-page.js
+++ leaderboard-react/frontend/tests/pages/leaderboard-page.js
@@ -12,93 +12,32 @@ export class LeaderboardPage {
     async open(mission_id) {
         if (mission_id) {
             await this.page.goto(`http://localhost/?mission_id=${mission_id}`);
-            await this.titleHasMissionId(mission_id);
         } else {
             await this.page.goto('http://localhost');
         }
     }
 
-    async titleHasMissionId(id) {
-        await expect(this.title).toHaveText(new RegExp(`^${id} - .*`));
+    async getTitleText() {
+        return this.title.textContent();
     }
 
-    async hasVisibleScores() {
-        await expect(this.title).toHaveText(/^\d+ - .*/);
-        await expect(this.scores.first()).toBeVisible();
-        await expect(this.scores.first()).toContainText(':');
-    }
-
-    async scoresDisplayProperTimeFormat() {
-        await expect(this.scores.first()).toBeVisible();
-
+    async getScoresText() {
         const count = await this.scores.count();
+        const texts = [];
         for (let i = 0; i < count; i++) {
-            const text = await this.scores.nth(i).textContent();
-            const match = text.match(/: ^([0-9]|1[0-9]|2[0-3]):(?:[0-5])(?:[0-9])$/);
-            if (!match) {
-                return false;
-            }
+          texts.push(await this.scores.nth(i).textContent());
         }
-
+        return texts;
     }
 
-    async isSortedAscending() {
-        await expect(this.scores.first()).toBeVisible();
-
-        const times = [];
-        const count = await this.scores.count();
-        for (let i = 0; i < count; i++) {
-            const text = await this.scores.nth(i).textContent();
-            const match = text.match(/: ^([0-9]|1[0-9]|2[0-3]):(?:[0-5])(?:[0-9])$/);
-            if (match) {
-                times.push(parseInt(match[1], 10));
-            }
-        }
-
-        for (let i = 0; i < times.length - 1; i++) {
-            expect(times[i]).toBeLessThanOrEqual(times[i + 1]);
-            // await this.page.pause();
-        }
-    }
+    async clickPrevBtn() { await this.prevBtn.click(); }
+    async clickNextBtn() { await this.nextBtn.click(); }
 
     async getLastMissionId() {
-        const response = await fetch('http://localhost:4000/missions');
+        const response = await this.page.request.get('http://localhost:4000/missions');
         const missions = await response.json();
         const lastMissionId = missions.length > 0 ? missions[missions.length - 1].id : 1;
 
         return lastMissionId;
-    }
-
-    async onFirstMission() {
-        await expect(this.title).toHaveText(/^1 -/);
-    }
-
-    async onLastMission() {
-        const lastMissionId = await this.getLastMissionId();
-        await expect(this.title).toHaveText(new RegExp(`^${lastMissionId} -`));
-    }
-
-    async prevBtnDoesNotDecrement() {
-        let curTitle = this.title.text;
-        await this.prevBtn.click();
-        await this.title.text === curTitle;
-    }
-
-    async prevBtnDecrements() {
-        let curTitle = this.title.text;
-        await this.prevBtn.click();
-        await this.title.text !== curTitle;
-    }
-
-    async nextBtnDoesNotIncrement() {
-        let curTitle = this.title.text;
-        await this.nextBtn.click();
-        await this.title.text === curTitle;
-    }
-
-    async nextBtnDecrements() {
-        let curTitle = this.title.text;
-        await this.nextBtn.click();
-        await this.title.text !== curTitle;
     }
 }
